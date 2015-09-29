27bcd03f4404b3720cd4e40b47581f11dc05b12f
Alice <alice@example.com>
2015-09-29 20:42:13 +0900

    add a environment variable.

--- lib/model/JobeetJob.php
+++ lib/model/JobeetJob.php
@@ -54,7 +54,7 @@ class JobeetJob extends BaseJobeetJob {
     if ($this->isNew() && !$this->getExpiresAt())
     {
       $now = $this->getCreatedAt() ? $this->getCreatedAt('U') : time();
-      $this->setExpiresAt($now + 86400 * 30);
+      $this->setExpiresAt($now + 86400 * sfConfig::get('app_active_days'));
     }
 
     return parent::save($con);
