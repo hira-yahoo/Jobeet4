a8419d95f075ab5514b03c73398d9bfa15b76e21
Alice <alice@example.com>
2015-09-30 18:43:23 +0900

    add a propel test.
    finished the chapter08.

--- test/unit/JobeetTest.php
+++ test/unit/JobeetTest.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__).'/../bootstrap/unit.php';
 
 
-$t = new lime_test(6);
+$t = new lime_test(9);
 
 $t->comment('::slugify()');
 $t->is(Jobeet::slugify('Sensio'), 'sensio', '::slugify() converts all characters to lower case');
