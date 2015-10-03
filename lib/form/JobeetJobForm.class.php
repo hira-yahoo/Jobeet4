e722a3448557ef431fa4ea31ef47e2ba49f94ed4
Alice <alice@example.com>
2015-10-03 23:45:51 +0900

    add a validator to the form.

--- lib/form/JobeetJobForm.class.php
+++ lib/form/JobeetJobForm.class.php
@@ -12,5 +12,10 @@ class JobeetJobForm extends BaseJobeetJobForm
   public function configure()
   {
     $this->useFields(array('category_id', 'type', 'company', 'logo', 'url', 'position', 'location', 'description', 'how_to_apply', 'token', 'is_public', 'email'));
+
+    $this->validatorSchema['email'] = new sfValidatorAnd(array(
+      $this->validatorSchema['email'],
+      new sfValidatorEmail(),
+    ));
   }
 }
