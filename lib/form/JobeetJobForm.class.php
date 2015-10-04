21d6fa36513d37b5becb001c5b794768174a46d9
Alice <alice@example.com>
2015-10-04 18:51:27 +0900

    organize the form.

--- lib/form/JobeetJobForm.class.php
+++ lib/form/JobeetJobForm.class.php
@@ -11,28 +11,24 @@ class JobeetJobForm extends BaseJobeetJobForm
 {
   public function configure()
   {
-    $this->useFields(array('category_id', 'type', 'company', 'logo', 'url', 'position', 'location', 'description', 'how_to_apply', 'token', 'is_public', 'email'));
+    unset(
+      $this['created_at'], $this['updated_at'],
+      $this['expires_at'], $this['is_activated']
+    );
 
-    $this->validatorSchema['email'] = new sfValidatorAnd(array(
-      $this->validatorSchema['email'],
-      new sfValidatorEmail(),
-    ));
+    $this->validatorSchema['email'] = new sfValidatorEmail();
 
     $this->widgetSchema['type'] = new sfWidgetFormChoice(array(
-      'choices'  => JobeetJobPeer::$types,
+      'choices' => JobeetJobPeer::$types,
       'expanded' => true,
     ));
-
-
     $this->validatorSchema['type'] = new sfValidatorChoice(array(
       'choices' => array_keys(JobeetJobPeer::$types),
     ));
 
-
     $this->widgetSchema['logo'] = new sfWidgetFormInputFile(array(
       'label' => 'Company logo',
     ));
-
 
     $this->widgetSchema->setLabels(array(
       'category_id'    => 'Category',
@@ -40,13 +36,11 @@ class JobeetJobForm extends BaseJobeetJobForm
       'how_to_apply'   => 'How to apply?',
     ));
 
-
     $this->validatorSchema['logo'] = new sfValidatorFile(array(
       'required'   => false,
       'path'       => sfConfig::get('sf_upload_dir').'/jobs',
       'mime_types' => 'web_images',
     ));
-
 
     $this->widgetSchema->setHelp('is_public', 'Whether the job can also be published on affiliate websites or not.');
   }
